030ecc8713483050a3dad1057f1b9ba8af2c11ca
Oleg Zhurakousky <ozhurakousky@vmware.com>
2012-03-04 19:11:53 -0500

    polished OrderProcessingApp to remove call to 'inavlidOrder'

--- src/main/scala/spring/integration/scala/dsl/OrderProcessingApp.scala
+++ src/main/scala/spring/integration/scala/dsl/OrderProcessingApp.scala
@@ -5,49 +5,48 @@ import java.util.concurrent.Executors
 
 /**
  * OrderProcessing sample
- * 
+ *
  * @author Oleg Zhurakousky
  */
 object OrderProcessingApp {
- 
+
   def main(args: Array[String]): Unit = {
-    
+
     val validOrder = PurchaseOrder(List(
-    PurchaseOrderItem("books", "Spring Integration in Action"),
-    PurchaseOrderItem("books", "DSLs in Action"),
-    PurchaseOrderItem("bikes", "Canyon Torque FRX")))
+      PurchaseOrderItem("books", "Spring Integration in Action"),
+      PurchaseOrderItem("books", "DSLs in Action"),
+      PurchaseOrderItem("bikes", "Canyon Torque FRX")))
 
-  val invalidOrder = PurchaseOrder(List())
+    val invalidOrder = PurchaseOrder(List())
 
-  val errorFlow = handle.using { m: Message[_] => println("Received ERROR: " + m); "ERROR processing order" }
- 
-  val aggregateOrder = aggregate()
+    val errorFlow = handle.using { m: Message[_] => println("Received ERROR: " + m); "ERROR processing order" }
 
-  val processBikeOrder =
-    handle.using { m: Message[_] => println("Processing bikes order: " + m); m } -->
-    aggregateOrder
+    val aggregateOrder = aggregate()
 
-  val orderProcessingFlow =
-      filter.using{p: PurchaseOrder => !p.items.isEmpty}.where(exceptionOnRejection = true) -->
-      split.using { p: PurchaseOrder => p.items } -->
-      Channel.withDispatcher(taskExecutor = Executors.newCachedThreadPool) -->
-      route.using { pi: PurchaseOrderItem => pi.itemType }(
-        when("books") then
-          handle.using { m: Message[_] => println("Processing books order: " + m); m } -->
-          aggregateOrder,
-        when("bikes") then
-          processBikeOrder
-      )
+    val processBikeOrder =
+      handle.using { m: Message[_] => println("Processing bikes order: " + m); m } -->
+        aggregateOrder
 
-  val validOrderResult = orderProcessingFlow.sendAndReceive[Any](validOrder, errorFlow = errorFlow)
-  println("Result: " + validOrderResult)
- 
-  val invalidOrderResult = orderProcessingFlow.sendAndReceive[Any](invalidOrder, errorFlow = errorFlow)
-  println("Result: " + invalidOrderResult)
+    val orderProcessingFlow =
+        filter.using { p: PurchaseOrder => !p.items.isEmpty }.where(exceptionOnRejection = true) -->
+        split.using { p: PurchaseOrder => p.items } -->
+        Channel.withDispatcher(taskExecutor = Executors.newCachedThreadPool) -->
+        route.using { pi: PurchaseOrderItem => pi.itemType }(
+          when("books") then
+            handle.using { m: Message[_] => println("Processing books order: " + m); m } -->
+            aggregateOrder,
+          when("bikes") then
+            processBikeOrder)
+
+    val validOrderResult = orderProcessingFlow.sendAndReceive[Any](validOrder, errorFlow = errorFlow)
+    println("Result: " + validOrderResult)
     
+    // un-comment the code below to see message processing during error condition
+
+//    val invalidOrderResult = orderProcessingFlow.sendAndReceive[Any](invalidOrder, errorFlow = errorFlow)
+//    println("Result: " + invalidOrderResult)
   }
 }
-
 
 case class PurchaseOrder(val items: List[PurchaseOrderItem])
 
